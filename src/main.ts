36f34b47ddbd5e5b8d37e17bc8d883fcab0643c1
Manohar <mramarao@deloitte.com>
2020-11-25 10:13:12 +0530

    Converted to GRPC microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import { microserviceOptions } from './config/microservice.config';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  /* const app = await NestFactory.create(AppModule);
+  await app.listen(3000); */
+  const logger = new Logger('UserManagementMicroservice');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    microserviceOptions,
+  );
+
+  await app.listen(() => {
+    logger.debug(`UMS is listening on port ${microserviceOptions.options['url']}`);
+  });
 }
 bootstrap();
