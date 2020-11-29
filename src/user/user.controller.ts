007e39e94d263ad2a9ed5996bba3835d118dc264
Manohar <mramarao@deloitte.com>
2020-11-29 13:35:37 +0530

    Implemented global rpc exceptoin filter.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Logger, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Logger, UseFilters, UsePipes, ValidationPipe } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import { User } from './model/user.model';
 import { UserService } from './user.service';
 // import { SignUpValidationPipe } from './pipes/signup-req-validation.pipe';
+import { AllRpcExceptionsFilter } from '../utility/filters/rpc-exception.filter';
 
 @Controller('user')
+@UseFilters(AllRpcExceptionsFilter)
 export class UserController {
 
-    private logger = new Logger('UserController');
+    private logger = new Logger(UserController.name);
     constructor(
         private userService: UserService
     ){}
@@ -20,5 +22,9 @@ export class UserController {
         user = await this.userService.signUp(body);
         return user;
     }
+
+    /* createCreds(req: CreateCredsReqDto): Promise<void>{
+        return;
+    } */
 
 }
---
+++ src/utility/filters/rpc-exception.filter.ts
@@ -0,0 +1,39 @@
+import { Catch, RpcExceptionFilter, ArgumentsHost, Logger, BadRequestException, BadGatewayException, ForbiddenException } from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { BaseRpcExceptionFilter, RpcException } from '@nestjs/microservices';
+import { error } from 'console';
+
+// @Catch()
+// export class GrpcExceptionFilter implements RpcExceptionFilter<RpcException> {
+
+//   // private readonly logger: LoggerService = new LoggerService(GrpcExceptionFilter.name);
+//   private readonly logger = new Logger('GrpcExceptionFilter');
+
+//   catch(exception: RpcException, host: ArgumentsHost): Observable<any> {
+
+//     const err = exception.getError();
+//     this.logger.debug(`Inside GrpcExceptionFilter`);
+//     this.logger.error(JSON.stringify(exception.getError()), exception.stack);
+//     // If an unfamiliar error occurs / not a business error
+//    /*  if (Object.values(BusinessErrors).includes(err.toString()) == false) {
+//       this.logger.error(JSON.stringify(exception.getError()), exception.stack);
+//     } */
+//     return throwError(exception.getError());
+//   }
+// }
+
+@Catch()
+export class GlobalRpcExceptionsFilter extends BaseRpcExceptionFilter {
+
+  private readonly logger = new Logger(GlobalRpcExceptionsFilter.name);
+  
+  catch(exception: any, host: ArgumentsHost): Observable<any> {
+    this.logger.debug(`Inside ${GlobalRpcExceptionsFilter.name}`);
+    this.logger.error(exception, exception.stack);
+    // Below line will return only the message.
+    // return throwError(new RpcException(exception.response.message[0]));
+    
+    // Below line will return entire object in JSON string
+    return throwError(new RpcException((JSON.stringify(exception))));
+  }
+}
